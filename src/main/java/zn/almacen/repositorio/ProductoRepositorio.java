717a023bb9093e843609b9a387cd2dd55e9ab89e
valen <rodrigeuzvalentin@gmail.com>
2025-03-12 10:46:33 -0300

    Arregle errores

--- src/main/java/zn/almacen/repositorio/ProductoRepositorio.java
+++ src/main/java/zn/almacen/repositorio/ProductoRepositorio.java
@@ -3,8 +3,5 @@ package zn.almacen.repositorio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import zn.almacen.modelo.Producto;
 
-import java.util.List;
-
 public interface ProductoRepositorio extends JpaRepository<Producto,Integer> {
-    List<Producto> findByCategoria(String categoria);
 }
