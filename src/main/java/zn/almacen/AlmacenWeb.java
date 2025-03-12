7515551d2a1a1116a9dfebe0571111fccc80ca30
valen <rodrigeuzvalentin@gmail.com>
2025-03-11 23:06:53 -0300

    activa el tomcat para poder ver la pagina

---
+++ src/main/java/zn/almacen/AlmacenWeb.java
@@ -0,0 +1,11 @@
+package zn.almacen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmacenWeb {
+    public static void main(String[] args) {
+        SpringApplication.run(AlmacenWeb.class, args);
+    }
+}
