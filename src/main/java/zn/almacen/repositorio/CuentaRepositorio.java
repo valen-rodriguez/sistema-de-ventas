00a4a4fc1e195720c61001017eea4434f7434e61
valen <rodrigeuzvalentin@gmail.com>
2025-03-17 21:02:42 -0300

    Arregle errores

--- src/main/java/zn/almacen/repositorio/CuentaRepositorio.java
+++ src/main/java/zn/almacen/repositorio/CuentaRepositorio.java
@@ -3,6 +3,8 @@ package zn.almacen.repositorio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import zn.almacen.modelo.Cuenta;
 
+import java.util.Optional;
+
 public interface CuentaRepositorio extends JpaRepository<Cuenta, Integer> {
-    Cuenta findCuentaByMail(String mail);
+    Optional<Cuenta> findCuentaByMail(String mail);
 }
