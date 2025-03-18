35a2de8e5db7681dfacea01099aa75736cf17187
valen <rodrigeuzvalentin@gmail.com>
2025-03-17 22:04:20 -0300

    Terminé el apartado del login

--- src/main/java/zn/almacen/controlador/LoginControlador.java
+++ src/main/java/zn/almacen/controlador/LoginControlador.java
@@ -1,10 +1,7 @@
 package zn.almacen.controlador;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.stage.Stage;
@@ -16,8 +13,6 @@ import org.springframework.stereotype.Component;
 import zn.almacen.servicio.CuentaServicio;
 import javafx.scene.control.*;
 
-import java.awt.event.ActionEvent;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
