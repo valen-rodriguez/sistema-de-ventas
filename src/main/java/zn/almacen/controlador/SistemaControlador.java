b5e6046d262bba6293689734d91b9e93400cf828
valen <rodrigeuzvalentin@gmail.com>
2025-03-18 09:28:28 -0300

    Empiezo el apartado del sistema principal

---
+++ src/main/java/zn/almacen/controlador/SistemaControlador.java
@@ -0,0 +1,20 @@
+package zn.almacen.controlador;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.springframework.stereotype.Component;
+import zn.almacen.modelo.Cuenta;
+
+import javafx.scene.control.*;
+
+@Component
+public class SistemaControlador extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+
+}
