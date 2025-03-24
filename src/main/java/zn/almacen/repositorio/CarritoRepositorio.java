c44e213c783c8e4f4a6c95c407a4bd405304f758
valen-rodriguez <rodriguezvalentin1205@gmail.com>
2025-03-24 19:54:20 -0300

    Borre las clases del carrito porque no hicieron falta

--- src/main/java/zn/almacen/repositorio/CarritoRepositorio.java
+++
@@ -1,7 +0,0 @@
-//package zn.almacen.repositorio;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import zn.almacen.modelo.Carrito;
-//
-//public interface CarritoRepositorio extends JpaRepository<Carrito,Integer> {
-//}
